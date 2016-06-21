eaccc9abb3685da010e53f8697139f15fb3caea7
William Dye <will2dye4@mac.com>
2016-06-21 00:06:50 -0400

    Removed unnecessary view for index page. Moved to Python 3 (no code changes necessary).

--- pensieve/pensieve/urls.py
+++ pensieve/pensieve/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^memories/', include('memories.urls'))
 ]
